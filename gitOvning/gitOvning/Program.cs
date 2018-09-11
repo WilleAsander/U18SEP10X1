22a17adff0518a5ee7d141bfec264b3eba30a9c2
WilleAsander <wasander13@gmail.com>
2018-09-11 16:06:00 +0200

    Övning 5 klar

--- gitOvning/gitOvning/Program.cs
+++ gitOvning/gitOvning/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace gitOvning
 {
     class Program
@@ -23,14 +24,14 @@ namespace gitOvning
                     string mening = Console.ReadLine();
                     string[] orden = mening.Split(' ');
                     Console.WriteLine("Det är " + orden.Count() + " ord i meningen!");
-                    int[] letters = new int [orden.Count()];
+                    int[] letters = new int[orden.Count()];
                     for (int i = 0; i < orden.Length; i++)
                     {
                         letters[i] = orden[i].Count();
-                        Console.WriteLine(orden[i] + " (" + letters[i] + ") " );
+                        Console.WriteLine(orden[i] + " (" + letters[i] + ") ");
                     }
                     Console.ReadLine();
-                break;
+                    break;
 
                 case "b":
                     Console.Write("Mata in en mening, vilken som helst: ");
@@ -55,11 +56,72 @@ namespace gitOvning
                 case "c":
                     Console.Write("Mata in en mening, vilken som helst: ");
                     mening = Console.ReadLine();
-                    orden = mening.Split(' ');
-                    awdawdasd
-                    
+                    string txt = mening.ToLower();
+                    orden = txt.Split(' ');
+
+                    var list = new List<string>(orden);
+                    var q = list.GroupBy(x => x)
+                                .Select(g => new { Value = g.Key, Count = g.Count() })
+                                .OrderByDescending(x => x.Count);
+                    foreach (var x in q)
+                    {
+                        Console.WriteLine("Ord: " + x.Value + " Antal: " + x.Count);
+                    }
+                    Console.ReadLine();
+                    break;
+
+                case "d":
+                    Console.Write("Mata in en mening, vilken som helst: ");
+                    mening = Console.ReadLine();
+                    txt = mening.ToLower();
+                    orden = txt.Split(new char[] { ' ' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                    char[] characters = string.Join(string.Empty, orden).ToCharArray();
+                    var character_query = (from char word in characters orderby word select word).Distinct();
+                    int ammountThree = character_query.Count();
+                    char[] resultTwo = character_query.ToArray();
+                    foreach (char ch in resultTwo)
+                    {
+                        int count = mening.Count(g => g == ch);
+                        Console.WriteLine(ch + "(" + count + ")");
+
+                    }
+                    Console.ReadLine();
+                    break;
+
+                case "e":
+                    string ord = "";
+                    string[] sentence = new string [100];
+                    bool run = true;
+                    var s = 0;
+                    while (run)                   
+                    {
+                        Console.Write("Mata in ett ord: ");
+                        ord = Console.ReadLine();                       
+                        s++;                       
+                        string small = ord.ToLower();
+                        
+                        if (small == "avsluta")
+                        {
+                            run = false;
+                            s = 0;
+                            sentence[s] = " ";
+                        }
+                        else
+                        {
+                            sentence[s] = small;
+                        }
+                    }
+
+                    Console.Write("Din mening blev: ");
+                    for (int i = 0; i < sentence.Length; i++)
+                    {
+                        Console.Write(sentence[i] + " ");
+                    }
+                    Console.ReadLine();
 
                     break;
+                
             } 
                 
         }
