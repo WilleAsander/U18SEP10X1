095b88051058555e067fe3da8e7ec7d0f57732db
WilleAsander <wasander13@gmail.com>
2018-09-11 16:26:52 +0200

    Alla övningar klara!

--- gitOvning/gitOvning/Program.cs
+++ gitOvning/gitOvning/Program.cs
@@ -146,6 +146,47 @@ namespace gitOvning
                     }
                     Console.ReadLine();
                     break;
+
+                case "g":
+                    Console.Write("Skriv en meing: ");
+                    string sentenceSix = Console.ReadLine().ToUpper();
+                    string newSentence = "";
+                    Dictionary<string, string> leet = new Dictionary<string, string>();
+                    leet.Add("A", @"/-\");
+                    leet.Add("B", @"|3");
+                    leet.Add("C", @"(");
+                    leet.Add("D", @"|)");
+                    leet.Add("E", @"3");
+                    leet.Add("F", @"|=");
+                    leet.Add("G", @"(");
+                    leet.Add("H", @"|-|");
+                    leet.Add("I", @"l");
+                    leet.Add("J", @"    _|");
+                    leet.Add("K", @"|<");
+                    leet.Add("L", @"|_");
+                    leet.Add("M", @"|\/|");
+                    leet.Add("N", @"|\|");
+                    leet.Add("O", @"0");
+                    leet.Add("P", @"|2");
+                    leet.Add("Q", @"(,)");
+                    leet.Add("R", @"|2");
+                    leet.Add("S", @"5");
+                    leet.Add("T", @"+");
+                    leet.Add("U", @"|_|");
+                    leet.Add("V", @"    |/");
+                    leet.Add("W", @"\/\/");
+                    leet.Add("X", @"><");
+                    leet.Add("Y", @"`/");
+                    leet.Add("Z", @"2");
+
+                    foreach (KeyValuePair<string, string> x in leet)
+                    {
+                        sentenceSix = sentenceSix.Replace(x.Key, x.Value);
+                    }
+                    Console.WriteLine("Your sentence in leet is: " + sentenceSix);
+                    Console.ReadLine();
+                    break;
+             
             } 
                 
         }
